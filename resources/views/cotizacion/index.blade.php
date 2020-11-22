b783943a46896be94b7800d16bb5b52378323a9c
mviza <macarena@MacBook-Pro-de-Macarena.local>
2020-11-22 02:53:44 -0300

    eliminar algunos campos de vista de cotizaciones

--- resources/views/cotizacion/index.blade.php
+++ resources/views/cotizacion/index.blade.php
@@ -15,11 +15,6 @@
                 <tr>
                     <th>id</th>
                     <th>Rut Cliente</th>
-                    <th>Creación</th>
-                    <th>Modificación</th>
-                    <th>Estado</th>
-                    <th>Crédito</th>
-                    <th>Monto Crédito</th>
                     <th>Subtotal</th>
                     <th>Descuento</th>
                     <th>Total</th>
@@ -33,11 +28,6 @@
                 <tr>
                     <td> {{ $cotizacion->idCotizacion }} </td>
                     <td> {{ $cotizacion->cliente->rut }} </td>
-                    <td> {{ $cotizacion->fechaCreacion }} </td>
-                    <td> {{ $cotizacion->fechaModificacion }} </td>
-                    <td> {{ $cotizacion->estado }} </td>
-                    <td> {{ $cotizacion->credito }} </td>
-                    <td> {{ $cotizacion->montoCredito }} </td>
                     <td> {{ $cotizacion->subtotal}}</td>
                     <td> {{ $cotizacion->descuento}}</td>
                     <td> {{ $cotizacion->total}}</td>
